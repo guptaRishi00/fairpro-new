bd24e0b9923ceaed60c9af1aea60510d37c96766
guptaRishi00 <rishigupta54066@gmail.com>
2025-11-08 15:24:58 +0530

    feat: email service

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer({}: Props) {
   return (
     <div className="w-full bg-black">
       <Image
-        src={"/footer2.png"}
+        src={"/footer3.png"}
         alt="hh"
         width={1500}
         height={1500}
